45eb87f1b1d7a2407f43f2b9250020303225e8f8
Ragav-KS <57057039+Ragav-KS@users.noreply.github.com>
2025-03-29 18:09:10 +0530

    chore(repo): update gitignore and fix line endings

--- tools/eslint-config/index.js
+++ tools/eslint-config/index.js
@@ -3,6 +3,6 @@ import { configs as tsESlintConfigs } from 'typescript-eslint';
 
 export const baseConfig = [
   eslint.configs.recommended,
-  tsESlintConfigs.strict,
-  tsESlintConfigs.stylistic,
+  ...tsESlintConfigs.strict,
+  ...tsESlintConfigs.stylistic,
 ];
