34b72aec75e3b43e0e14d56db4f55defd83271eb
Ragav-KS <57057039+Ragav-KS@users.noreply.github.com>
2025-04-06 15:05:47 +0530

    feat(backend-lambda): verify connectivity from lambda to dynamodb

--- apps/backend-lambda/src/index.ts
+++ apps/backend-lambda/src/index.ts
@@ -1,27 +1,6 @@
 import { Hono } from 'hono';
 import { handle } from 'hono/aws-lambda';
-
-const webSocketDefaultRoute = new Hono();
-
-webSocketDefaultRoute.post('/create-room', async (c) => {
-  return c.json({
-    message: 'Room created',
-  });
-});
-
-const websocketRoute = new Hono();
-
-websocketRoute
-  .get('/connect', async (c) => {
-    console.log('Connect route called');
-    return c.status(200);
-  })
-  .delete('/connect', async (c) => {
-    console.log('Disconnect route called');
-    return c.status(200);
-  });
-
-websocketRoute.route('/default', webSocketDefaultRoute);
+import { websocketRoute } from './routes/websocketRoute';
 
 export const app = new Hono();
 
---
+++ apps/backend-lambda/src/routes/websocketRoute.ts
@@ -0,0 +1,22 @@
+import { Hono } from 'hono';
+import { webSocketDefaultRoute } from './websocket/defaultRoute';
+
+export const websocketRoute = new Hono();
+
+websocketRoute.onError(async (err, c) => {
+  console.error(err);
+
+  return c.json({ message: 'Internal Server Error' });
+});
+
+websocketRoute
+  .get('/connect', async (c) => {
+    console.log('Connect route called');
+    return c.text('connected');
+  })
+  .delete('/connect', async (c) => {
+    console.log('Disconnect route called');
+    return c.text('disconnected');
+  });
+
+websocketRoute.route('/default', webSocketDefaultRoute);
