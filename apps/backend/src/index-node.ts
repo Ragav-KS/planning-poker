f03e183e5a5a2ac86a37fadfab43c0a691f7bef7
Ragav-KS <57057039+Ragav-KS@users.noreply.github.com>
2025-04-09 01:56:04 +0530

    feat(backend): pass the authentication context to the handler

--- apps/backend/src/index-node.ts
+++ apps/backend/src/index-node.ts
@@ -1,6 +1,7 @@
 import { serve } from '@hono/node-server';
 import { createNodeWebSocket } from '@hono/node-ws';
 import { Hono } from 'hono';
+import { jwt } from 'hono/jwt';
 import { app } from '.';
 
 const localApp = new Hono();
@@ -13,7 +14,10 @@ localApp.route('/', app);
 
 localApp.get(
   '/',
-  upgradeWebSocket(() => {
+  jwt({ secret: process.env.APP_JWT_SECRET_KEY }),
+  upgradeWebSocket((c) => {
+    const authPayload = c.get('jwtPayload');
+
     return {
       async onOpen() {
         await app.request(`/websocket/connect`, {
@@ -25,13 +29,15 @@ localApp.get(
       async onMessage(msg, ws) {
         const messageJson = JSON.parse(msg.data.toString());
 
-        const res = await app.request(
-          `/websocket/default/${messageJson['action']}`,
-          {
-            method: 'POST',
-            body: JSON.stringify(messageJson['data']),
-          },
-        );
+        const action = messageJson['action'];
+        const data = messageJson['data'];
+
+        data['auth'] = authPayload;
+
+        const res = await app.request(`/websocket/default/${action}`, {
+          method: 'POST',
+          body: JSON.stringify(data),
+        });
 
         const responseBody = await res.json();
 
--- apps/backend/src/routes/websocket/defaultRoute.ts
+++ apps/backend/src/routes/websocket/defaultRoute.ts
@@ -3,6 +3,10 @@ import { Hono } from 'hono';
 export const webSocketDefaultRoute = new Hono();
 
 webSocketDefaultRoute.post('/vote', async (c) => {
+  const body = await c.req.json();
+
+  console.log(body);
+
   return c.json({
     message: 'test',
   });
