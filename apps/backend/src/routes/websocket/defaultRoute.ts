2568c88afa8d9e6e9a5d01322f77eb0b680b02f6
Ragav-KS <57057039+Ragav-KS@users.noreply.github.com>
2025-04-10 02:46:20 +0530

    feat(backend): implement the vote message handler

--- apps/backend/src/routes/websocket/defaultRoute.ts
+++ apps/backend/src/routes/websocket/defaultRoute.ts
@@ -1,13 +1,38 @@
+import { UpdateCommand } from '@aws-sdk/lib-dynamodb';
+import { zValidator } from '@hono/zod-validator';
+import { z } from 'zod';
+import { USERS_TABLE } from '../../constants';
 import { factory } from '../../hono-factory';
+import { docClient } from '../../services/aws-sdk/ddbClient';
 
 export const webSocketDefaultRoute = factory.createApp();
 
-webSocketDefaultRoute.post('/vote', async (c) => {
-  const body = await c.req.json();
+webSocketDefaultRoute.post(
+  '/vote',
+  zValidator(
+    'json',
+    z.object({
+      value: z.number(),
+    }),
+  ),
+  async (c) => {
+    const userId = c.env.requestContext.authorizer.principalId;
 
-  console.log(body);
+    const voteValue = c.req.valid('json').value;
 
-  return c.json({
-    message: 'test',
-  });
-});
+    const command = new UpdateCommand({
+      TableName: USERS_TABLE,
+      Key: {
+        userId,
+      },
+      UpdateExpression: 'set vote = :vote',
+      ExpressionAttributeValues: {
+        ':vote': voteValue,
+      },
+    });
+
+    await docClient.send(command);
+
+    return c.body(null, 202);
+  },
+);
