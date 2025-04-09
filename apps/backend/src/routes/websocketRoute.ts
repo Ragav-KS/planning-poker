c4df86284ec5712683d5525f6f3ccc602018712b
Ragav-KS <57057039+Ragav-KS@users.noreply.github.com>
2025-04-10 03:24:31 +0530

    feat(backend): store connection id when websocket connection is made

--- apps/backend/src/routes/websocketRoute.ts
+++ apps/backend/src/routes/websocketRoute.ts
@@ -1,12 +1,31 @@
+import { UpdateCommand } from '@aws-sdk/lib-dynamodb';
+import { USERS_TABLE } from '../constants';
 import { factory } from '../hono-factory';
+import { docClient } from '../services/aws-sdk/ddbClient';
 import { webSocketDefaultRoute } from './websocket/defaultRoute';
 
 export const websocketRoute = factory.createApp();
 
 websocketRoute
   .post('/connect', async (c) => {
-    console.log('Connect route called');
-    return c.text('connected');
+    const userId = c.env.requestContext.authorizer.principalId;
+
+    const connectionId = c.req.header('X-WebSocket-Connection-Id');
+
+    const command = new UpdateCommand({
+      TableName: USERS_TABLE,
+      Key: {
+        userId,
+      },
+      UpdateExpression: 'set connectionId = :connectionId',
+      ExpressionAttributeValues: {
+        ':connectionId': connectionId,
+      },
+    });
+
+    await docClient.send(command);
+
+    return c.body(null, 202);
   })
   .delete('/connect', async (c) => {
     console.log('Disconnect route called');
